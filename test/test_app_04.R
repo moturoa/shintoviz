2a7c2be1f8e68d73b7cca72e64e9be3165b58971
Remko Duursma <remko@shintolabs.nl>
2023-06-05 11:13:52 +0200

    widget height arg

--- test/test_app_04.R
+++ test/test_app_04.R
@@ -29,16 +29,16 @@ my_table_format_fun1 <<- function(data){
   data$`%` <- round(100 * data[[2]] / sum(data[[2]]), 2)
   data
 }
- 
+
 
 
 
 ui <- softui::simple_page(
-  
+
   softui::fluid_row(
     column(12,
     softui::box(title="Instellingen",closed=TRUE,
-                 
+
                 uiOutput("defaultSizes"),
                 action_button("updateButton", label= "Update")
                 )
@@ -47,41 +47,41 @@ ui <- softui::simple_page(
   softui::fluid_row(
     uiOutput("viz")
   )
-  
-  
+
+
 )
 
 server <- function(input, output, session) {
-  
-  defaultPointSize <- reactiveVal(3) 
-  defaultLabelSize <- reactiveVal(14) 
-  
+
+  defaultPointSize <- reactiveVal(3)
+  defaultLabelSize <- reactiveVal(14)
+
   observeEvent(input$updateButton,ignoreInit = FALSE, ignoreNULL = FALSE,{
- 
+
     req(input$pointSize)
     defaultPointSize(input$pointSize)
     defaultLabelSize(input$labelSize)
-  }) 
-  
+  })
+
    output$defaultSizes <- renderUI({
- 
+
        softui::fluid_row(
          numericInput(session$ns("pointSize"), label = "Punt grootte", value=defaultPointSize()),
          numericInput(session$ns("labelSize"), label = "Label grootte", value=defaultLabelSize())
        )
- 
+
   })
-  
+
   output$viz <- renderUI({
-   softui::fluid_row( 
-      column(4, 
-         plotWidgetUI(session$ns("plot1"),
-                      
+   softui::fluid_row(
+      column(4,
+         plotWidgetUI(session$ns("plot1"), height = 500, export = TRUE,
+
             header_ui = shintoshiny::select_input("sel_continent", NULL,
                                                   choices = unique(gapminder$continent),
                                                   selected = unique(gapminder$continent),
                                                   multiple = TRUE),
-            
+
             settingsUI = softui::fluid_row(tags$p( "Grafiek instellingen" ),
                                         selectInput("num_year", "Kies jaar data", choices = unique(gapminder$year)),
                                         numericInput("num_hjust", "Label hjust", value = -0.19),
@@ -89,7 +89,7 @@ server <- function(input, output, session) {
                                         numericInput("num_barwidth", "Bar width", value = 0.62),
                                         numericInput("num_basesize", "Base size", value = defaultLabelSize())
               )
-           
+
          )
       ),
       column(4,
@@ -101,34 +101,34 @@ server <- function(input, output, session) {
                                         numericInput("num_pointsize2", "In-plot size", value = defaultLabelSize()),
                                         numericInput("num_pointsize", "Point size", value = defaultPointSize())
             )
-       
+
          )
       ),
-      column(4, 
-             plotWidgetUI(session$ns("plot3"),   
+      column(4,
+             plotWidgetUI(session$ns("plot3"),
                   settingsUI =  softui::fluid_row(tags$p( "Grafiek instellingen" ),
                                               numericInput("num_basesize_3", "Base size", value = defaultLabelSize()),
                                               selectInput("sel_plot_type_3", "Type", choices = c("lines","stacked_bars","grouped_bars")),
                                               numericInput("num_pointsize_3", "Point size", value = defaultPointSize())
                   )
-               
-             ) 
+
+             )
       )
     )
   })
-  
-  
+
+
   raw_data <- reactive({
     gapminder
   })
-  
+
   raw_data_filtered <- reactive({
-    
+
     raw_data() %>%
       filter(continent %in% input$sel_continent)
-    
+
   })
-  
+
   callModule(plotWidgetModule, "plot1",
              data = raw_data_filtered,
              plot_type = reactive("plot_horizontal_bars"),
@@ -156,15 +156,15 @@ server <- function(input, output, session) {
                )
              )
   )
-  
-  
-  
+
+
+
   plot_data_2 <- reactive({
     gapminder %>%
       filter(country == "Netherlands") %>%
       mutate(pop = round(1e-06* pop,1))
   })
-  
+
   callModule(plotWidgetModule, "plot2",
              data = plot_data_2,
              plot_type = reactive("plot_value_by_time"),
@@ -187,15 +187,15 @@ server <- function(input, output, session) {
                )
              )
   )
-  
-  
-  
+
+
+
   plot_data_3 <- reactive({
     gapminder %>%
       filter(country %in% c("Netherlands","Belgium","Luxembourg","Denmark")) %>%
       mutate(pop = 1e-06* pop)
   })
-  
+
   callModule(plotWidgetModule, "plot3",
              data = plot_data_3,
              plot_type = reactive("plot_grouped_value_by_time"),
@@ -218,8 +218,8 @@ server <- function(input, output, session) {
                )
              )
   )
-  
-  
+
+
 }
 
 shinyApp(ui, server)
